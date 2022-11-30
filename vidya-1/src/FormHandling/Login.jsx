d0630978c8595f4a8c4a159cf6f4a2a45e53a971
VidyaChandrub <vidyac266@gmail.com>
2022-11-30 21:34:13 +0530

    okie

--- vidya-1/src/FormHandling/Login.jsx
+++ vidya-1/src/FormHandling/Login.jsx
@@ -5,6 +5,15 @@ class Login extends React.Component{
         email:"",
         password:""
     }
+    emailHandler = (event) => {
+        this.setState({email:event.target.value})
+    }
+    passwordHandler = (event) => {
+        this.setState({password:event.target.value})
+    }
+    submitHandler = (event) => {
+        alert(`${this.state.email}`)
+    }
 render(){
     return(
         <div><h1 className="text-danger">Form handling in jsx!</h1>
@@ -12,9 +21,9 @@ render(){
         <br/><br/>
         <form>
         <label>Email Id</label>
-        <input type="text" /><br/><br/>
+        <input type="text" name="email" value={this.state.email} onChange={this.emailHandler}/><br/><br/>
         <label>Password</label>
-        <input type="text" /><br/><br/>
+        <input type="text" name="password" value={this.state.password} onChange={this.passwordHandler}/><br/><br/>
         <input type="submit" value="Login" className="btn btn-success"/>
         </form>
         </div>
