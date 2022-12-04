3088fa21816a500ad44646f639915be01c9c2708
VidyaChandrub <vidyac266@gmail.com>
2022-12-04 19:54:01 +0530

    user

---
+++ vidya-1/src/Axios/User.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+class User extends React.Component{
+render(){
+    return(
+        <div>
+            <pre>{JSON.stringify()}</pre>
+            <div className="row">
+                <div className="col-md-12">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
+}
+export default User 
