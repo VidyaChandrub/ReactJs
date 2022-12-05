cf8d2f358553d8723ec7165683ab78a0bb078579
VidyaChandrub <vidyac266@gmail.com>
2022-12-05 20:59:00 +0530

    routing

--- vidya-1/src/App.js
+++ vidya-1/src/App.js
@@ -1,36 +1,10 @@
-/* import ClsFun1 from "./ClsFun1" */
-/* import FunCls1 from "./FunCls1" */
-/* import State from "./State" */
-/* import StateEx from "./StateEx" 
-import Navbar from './Navbar'
-import ShoppingList from './Examples/ShoppingList' */
-/* import Example1 from './Examples/Example1' */
-/* import Product from './Product/Product'
-import Eventbind from './EventBindig/Eventbind' */
-/* import Movie from './Movie' */
-/* import Employee from './Employee/Employee' */
-/* import Login from "./FormHandling/Login" */
-/* import Registration from './FormHandling/Registration' */
-import User from './Axios/User'
+import Navbar from './Routing/Navbar'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 let App = () => {
 
-    return <div>
-                                                                       {/* <h1>Good Morning</h1> */}
-                                                                       {/* <h2>Good Afternoon</h2> */}
-                                                                        {/* <ClsFun1 /> */}
-                                                                        {/* <FunCls1 /> */}
-                                                                        {/* <State /> */}
-                                                                       {/*  <Navbar />
-    <StateEx />
-    <ShoppingList /> */}
-    {/* <Example1 /> */}
-   {/*  <Product />
-    <Eventbind /> */}
-    {/* <Employee /> */}
-    {/* <Login /> */}
-    {/* <Registration /> */}
-    <User />
-    </div>
-    
+    return <>
+    <BrowserRouter></BrowserRouter>
+                    <Navbar />
+            </>
 }
 export default App
