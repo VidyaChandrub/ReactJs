4eb695db43b53b376fd20fcfff88b5e348ab69b0
VidyaChandrub <vidyac266@gmail.com>
2022-12-06 22:22:18 +0530

    hooks

--- vidya-1/src/Navbar/Navbar.jsx
+++ vidya-1/src/Navbar/Navbar.jsx
@@ -4,15 +4,15 @@ import {Link} from 'react-router-dom'
 class Navbar extends React.Component{
 
     render(){
-        return <nav>
-            <Link to="/">Navbar</Link>
-            <ul>
-                <li><Link to="/home">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to='/message'>Message</Link></li>
-                <li><Link to='/counter'>Counter</Link></li>
-                <li><Link to='/product'>Product</Link></li>
+        return <nav className="navbar navbar-dark navbar-expand-lg">
+            <Link className="navbar-brand " to="/">Navbar</Link>
+            <ul className="navbar-nav ml-auto ">
+                <li className="nav-item"><Link to="/home" className="nav-link">Home</Link></li>
+                <li className="nav-item"><Link to="/about" className="nav-link">About</Link></li>
+                <li className="nav-item"><Link to="/contact" className="nav-link">Contact</Link></li>
+                <li className="nav-item"><Link to='/message' className="nav-link">Message</Link></li>
+                <li className="nav-item"><Link to='/counter' className="nav-link">Counter</Link></li>
+                <li className="nav-item"><Link to='/product' className="nav-link">Product</Link></li>
             </ul>
 
         </nav>
