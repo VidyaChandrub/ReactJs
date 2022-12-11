cc4fe8e3f3e118612c25594e67315e8034aea105
VidyaChandrub <vidyac266@gmail.com>
2022-12-11 20:36:53 +0530

    contact

---
+++ vidya-1/src/Conatctapp/ContactApp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ContactApp extends Component {
+  render() {
+    return (
+      <div>ContactApp</div>
+    )
+  }
+}
+
+export default ContactApp
